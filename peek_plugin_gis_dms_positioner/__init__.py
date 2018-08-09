a555873a0335aeba6a1dcd1a174b660be134f68f
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-08-10 07:56:21 +1000

    Updated to version 1.1.1.2

--- peek_plugin_gis_dms_positioner/__init__.py
+++ peek_plugin_gis_dms_positioner/__init__.py
@@ -3,7 +3,7 @@
 from peek_plugin_base.client.PluginClientEntryHookABC import PluginClientEntryHookABC
 from typing import Type
 
-__version__ = '1.1.1'
+__version__ = '1.1.1.2'
 
 
 def peekClientEntryHook() -> Type[PluginClientEntryHookABC]:
