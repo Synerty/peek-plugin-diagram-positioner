181e646aea9f4274175a92091310be6fce9b5073
Jarrod Chesney <jarrod.chesney@synerty.com>
2020-12-15 20:39:00 +1000

    PEEK-591: Added peek pkg version dependencies to setup.py

--- setup.py
+++ setup.py
@@ -4,23 +4,25 @@ import shutil
 from setuptools import find_packages
 from setuptools import setup
 
+###############################################################################
+# Define variables
 #
 # Modify these values to fork a new plugin
 #
+
 author = "Synerty"
 author_email = 'contact@synerty.com'
 py_package_name = "peek_plugin_diagram_positioner"
 pip_package_name = py_package_name.replace('_', '-')
 package_version = '0.0.0'
-description = 'Peek Plugin DiagramPositioner - My first enhancement.'
+description = 'Peek Plugin Diagram Positioner.'
 
 download_url = 'https://bitbucket.org/synerty/%s/get/%s.zip'
 download_url %= pip_package_name, package_version
 url = 'https://bitbucket.org/synerty/%s' % pip_package_name
 
-#
-# Everything below should be ok
-#
+###############################################################################
+# Customise the package file finder code
 egg_info = "%s.egg-info" % pip_package_name
 if os.path.isdir(egg_info):
     shutil.rmtree(egg_info)
@@ -53,18 +55,34 @@ def find_package_files():
 
 package_files = find_package_files()
 
+###############################################################################
+# Define the dependencies
+
+# Ensure the dependency is the same major number
+# and no older then this version
+
+requirements = [
+    'peek-plugin-base',
+    'peek-plugin-diagram'
+]
+
 # Force the dependencies to be the same branch
 reqVer = '.'.join(package_version.split('.')[0:2]) + ".*"
 
 # >=2.0.*,>=2.0.6
-requirements = ['peek-plugin-base', 'peek-plugin-diagram']
-requirements = ["%s==%s,>=%s" % (pkg, reqVer, package_version) for pkg in requirements]
+requirements = ["%s==%s,>=%s" % (pkg, reqVer, package_version)
+                if pkg.startswith("peek") else pkg
+                for pkg in requirements]
+
+###############################################################################
+# Call the setuptools
 
 setup(
     name=pip_package_name,
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     package_data={'': package_files},
     install_requires=requirements,
+    zip_safe=False,
     version=package_version,
     description=description,
     author=author,
