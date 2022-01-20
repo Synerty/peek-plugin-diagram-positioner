e79dacc86410904252f849259a64c07ce4f0c218
Jarrod Chesney <jarrod.chesney@synerty.com>
2022-01-20 23:14:06 +1000

    PEEK-1381 Reformatted all code files with prettier
    The only alteration is 4 space indents instead of 2

--- peek_plugin_diagram_positioner/plugin-module/_private/PluginNames.ts
+++ peek_plugin_diagram_positioner/plugin-module/_private/PluginNames.ts
@@ -1,8 +1,10 @@
-export let diagramPositionerFilt = {"plugin": "peek_plugin_diagram_positioner"}
-export let diagramPositionerTuplePrefix = "peek_plugin_diagram_positioner."
+export let diagramPositionerFilt = { plugin: "peek_plugin_diagram_positioner" };
+export let diagramPositionerTuplePrefix = "peek_plugin_diagram_positioner.";
 
-export let diagramPositionerObservableName = "peek_plugin_diagram_positioner"
-export let diagramPositionerActionProcessorName = "peek_plugin_diagram_positioner"
-export let diagramPositionerTupleOfflineServiceName = "peek_plugin_diagram_positioner"
+export let diagramPositionerObservableName = "peek_plugin_diagram_positioner";
+export let diagramPositionerActionProcessorName =
+    "peek_plugin_diagram_positioner";
+export let diagramPositionerTupleOfflineServiceName =
+    "peek_plugin_diagram_positioner";
 
-export let diagramPositionerBaseUrl = "peek_plugin_diagram_positioner"
+export let diagramPositionerBaseUrl = "peek_plugin_diagram_positioner";
--- peek_plugin_diagram_positioner/plugin-module/_private/PositionOnDiagramService.ts
+++ peek_plugin_diagram_positioner/plugin-module/_private/PositionOnDiagramService.ts
@@ -1,18 +1,18 @@
-import { Injectable } from "@angular/core"
+import { Injectable } from "@angular/core";
 import {
     DocDbPopupActionI,
     DocDbPopupContextI,
     DocDbPopupService,
-    DocDbPopupTypeE
-} from "@peek/peek_core_docdb"
+    DocDbPopupTypeE,
+} from "@peek/peek_core_docdb";
 import {
     DiagramCoordSetService,
     DiagramCoordSetTuple,
     DiagramPositionService,
-    DispKeyLocation
-} from "@peek/peek_plugin_diagram"
-import { NgLifeCycleEvents } from "@synerty/vortexjs"
-import { Router } from "@angular/router"
+    DispKeyLocation,
+} from "@peek/peek_plugin_diagram";
+import { NgLifeCycleEvents } from "@synerty/vortexjs";
+import { Router } from "@angular/router";
 
 /** Position On GIS Service
  *
@@ -23,132 +23,132 @@ import { Router } from "@angular/router"
 export class PositionOnDiagramService extends NgLifeCycleEvents {
     // These are hard coded because of dependency issues
     private readonly MODEL_SETS = {
-        "pofDiagram": {name: "DMS Diagram", url: "peek_plugin_enmac_diagram"},
-        "gisDiagram": {name: "GIS Diagram", url: "peek_plugin_gis_diagram"},
-    }
-    
-    private coordSetsByModelSet: { [modelSetKey: string]: DiagramCoordSetTuple[] } = {}
-    private coordSetsNameLookup: { [key: string]: string } = {}
-    
+        pofDiagram: { name: "DMS Diagram", url: "peek_plugin_enmac_diagram" },
+        gisDiagram: { name: "GIS Diagram", url: "peek_plugin_gis_diagram" },
+    };
+
+    private coordSetsByModelSet: {
+        [modelSetKey: string]: DiagramCoordSetTuple[];
+    } = {};
+    private coordSetsNameLookup: { [key: string]: string } = {};
+
     constructor(
         private router: Router,
         private objectPopupService: DocDbPopupService,
         private positionService: DiagramPositionService,
         private coordSetService: DiagramCoordSetService
     ) {
-        super()
-        
+        super();
+
         this.objectPopupService
             .popupObservable(DocDbPopupTypeE.summaryPopup)
             .takeUntil(this.onDestroyEvent)
-            .subscribe((c: DocDbPopupContextI) => this.addLocateOnActions(c))
-        
+            .subscribe((c: DocDbPopupContextI) => this.addLocateOnActions(c));
+
         objectPopupService
             .popupObservable(DocDbPopupTypeE.detailPopup)
             .takeUntil(this.onDestroyEvent)
-            .subscribe((c: DocDbPopupContextI) => this.addLocateOnActions(c))
-        
+            .subscribe((c: DocDbPopupContextI) => this.addLocateOnActions(c));
+
         for (const modelSetKey of Object.keys(this.MODEL_SETS)) {
             this.coordSetService
                 .diagramCoordSetTuples(modelSetKey)
                 .takeUntil(this.onDestroyEvent)
                 .subscribe((coordSets: DiagramCoordSetTuple[]) => {
-                    this.coordSetsByModelSet[modelSetKey] = coordSets
-                    this.rebuildCoordSetNameMap()
-                })
+                    this.coordSetsByModelSet[modelSetKey] = coordSets;
+                    this.rebuildCoordSetNameMap();
+                });
         }
     }
-    
+
     private rebuildCoordSetNameMap(): void {
-        this.coordSetsNameLookup = {}
+        this.coordSetsNameLookup = {};
         for (const msKey of Object.keys(this.MODEL_SETS)) {
-            for (const coordSet of (this.coordSetsByModelSet[msKey] || [])) {
-                this.coordSetsNameLookup[`${msKey}-${coordSet.key}`]
-                    = coordSet.name
+            for (const coordSet of this.coordSetsByModelSet[msKey] || []) {
+                this.coordSetsNameLookup[`${msKey}-${coordSet.key}`] =
+                    coordSet.name;
             }
         }
     }
-    
-    private nameForCs(
-        modelSetKey: string,
-        coordSetKy: string
-    ): string {
-        return this.coordSetsNameLookup[`${modelSetKey}-${coordSetKy}`]
+
+    private nameForCs(modelSetKey: string, coordSetKy: string): string {
+        return this.coordSetsNameLookup[`${modelSetKey}-${coordSetKy}`];
     }
-    
-    private async addLocateOnActions(context: DocDbPopupContextI): Promise<void> {
-        const promises = []
-        const positions = []
-        
+
+    private async addLocateOnActions(
+        context: DocDbPopupContextI
+    ): Promise<void> {
+        const promises = [];
+        const positions = [];
+
         for (const modelSetKey of Object.keys(this.MODEL_SETS))
-            promises.push(this.loadPositions(modelSetKey, context, positions))
-        
-        await Promise.all(promises)
-        
-        if (positions.length == 0)
-            return
-        
+            promises.push(this.loadPositions(modelSetKey, context, positions));
+
+        await Promise.all(promises);
+
+        if (positions.length == 0) return;
+
         // If there is just one item, then just go straight there.
         if (positions.length == 1) {
-            const action = positions[0]
-            action.tooltip = action.name
-            action.name = null
-            action.icon = "environment"
-            context.addAction(action)
-            return
+            const action = positions[0];
+            action.tooltip = action.name;
+            action.name = null;
+            action.icon = "environment";
+            context.addAction(action);
+            return;
         }
-        
+
         context.addAction({
             name: null,
             tooltip: "Goto Other Diagram Locations",
             icon: "environment",
             callback: null,
             children: positions,
-        })
+        });
     }
-    
+
     private async loadPositions(
         modelSetKey: string,
         context: DocDbPopupContextI,
         positions: DocDbPopupActionI[]
     ): Promise<void> {
-        const coordSetKey = context.options.triggeredForContext
-        const dispKey = context.key
-        
-        const locations: DispKeyLocation[] = await this.positionService
-            .locationsForKey(modelSetKey, dispKey)
-        
+        const coordSetKey = context.options.triggeredForContext;
+        const dispKey = context.key;
+
+        const locations: DispKeyLocation[] =
+            await this.positionService.locationsForKey(modelSetKey, dispKey);
+
         for (const location of locations) {
-            
             // Ignore positioning on the same coord set
-            if (location.coordSetKey == coordSetKey)
-                continue
-            
-            const name = this.MODEL_SETS[modelSetKey].name + " - "
-                + this.nameForCs(modelSetKey, location.coordSetKey)
-            
+            if (location.coordSetKey == coordSetKey) continue;
+
+            const name =
+                this.MODEL_SETS[modelSetKey].name +
+                " - " +
+                this.nameForCs(modelSetKey, location.coordSetKey);
+
             positions.push({
                 name: name,
                 tooltip: null,
                 icon: null,
                 callback: () => this.updatePosition(location),
-                children: []
-            })
+                children: [],
+            });
         }
     }
-    
+
     private updatePosition(location: DispKeyLocation): void {
-        this.positionService.isReadyObservable()
+        this.positionService
+            .isReadyObservable()
             .first()
             .subscribe(() => {
                 this.positionService.positionByKey(
                     location.modelSetKey,
                     location.coordSetKey,
-                    {highlightKey: location.dispKey}
-                )
-            })
-        
-        this.router.navigate([this.MODEL_SETS[location.modelSetKey].url])
+                    { highlightKey: location.dispKey }
+                );
+            });
+
+        this.router.navigate([this.MODEL_SETS[location.modelSetKey].url]);
     }
-    
 }
